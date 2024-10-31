15b4cca9a7d6134b7c099503cf41a6fc0f75d711
NSyifaa <naylusyifa16@gmail.com>
2024-10-31 19:37:54 +0700

    GANTI PW

--- ganti_pass/gantipw.php
+++ ganti_pass/gantipw.php
@@ -43,7 +43,7 @@
   <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-light-primary elevation-4" style="background-color: #FFB6C1;">
+  <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #024CAA;">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="../auth/assets/img/logobk.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
