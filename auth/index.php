429e724776bbd2f878d2d1082129dfc05ba1ed11
NSyifaa <naylusyifa16@gmail.com>
2024-11-10 15:44:41 +0700

    merubah index auth

--- auth/index.php
+++ auth/index.php
@@ -1,70 +1,66 @@
 <?php
- 
-  require_once '../database/koneksi.php';
+require_once '../database/koneksi.php';
 ?>
 
-<!DOCTYPE html>
-<html class="no-js" lang="">
 
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>SIPENBUK</title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
-  <link rel="stylesheet" href="styles/app.min.css"/>
-  <link rel="shortcut icon" href="assets/img/logom.png">
- 
-</head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sistem Informasi Penjualan Buku</title>
 
-<body class="page-loading">
-  <!-- page loading spinner -->
-  <!-- <div class="pageload">
-    <div class="pageload-inner">
-      <div class="sk-rotating-plane"></div>
-    </div>
-  </div> -->
-  <!-- /page loading spinner -->
-  <div class="app signin v2 usersession">
-    <div class="session-wrapper">
-      <div class="session-carousel slide" data-ride="carousel" data-interval="3000">
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox" style="width: 100%; height: 100%;">
-          <div class="item active" style="background-image:url(assets/img/images.png);background-size: cover;background-repeat: no-repeat;background-position:center;  width: 100%; height: 100%;">
-          </div>
-           <div class="item" style="background-image:url(assets/img/book2.png);background-size:cover;background-repeat: no-repeat;background-position: center;  width: 100%; height: 100%;">
-          </div>
-          <div class="item" style="background-image:url(assets/img/bookstore.png);background-size:cover;background-repeat: no-repeat;background-position: center;  width: 100%; height: 100%;">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../assets_adminLTE/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="../assets_adminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../assets_adminLTE/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="#"><b> Sistem Informasi Penjualan Buku</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <form action="" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="username" name="username">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user-circle"></span>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="card bg-white  blue no-border" style="background-color:#FFB6C1;">
-        <div class="card-block">
-          <form role="form" class="form-layout" action="" method="post">
-            <div class="text-center m-b">    
-
-              <img src="assets/img/logobk.png" style='width:100px; height:100px;'/> 
-              <h4 class="text-uppercase"><b><font color="#000000">SISTEM INFORMASI PENJUALAN BUKU</font></b></h4>
-              <h4 class="text-uppercase"><font color="#000000">TOKO BUKU MAYANG</font></h4>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password" name="password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
             </div>
-            <div class="form-inputs p-b">
-              <label class="text-uppercase"><font color="#000000">Username</font></label>
-              <input type="username" class="form-control input-lg" name="username" id="username" placeholder="input username" required>
-              <label class="text-uppercase"><font color="#000000">Password</font></label>
-              <input type="password" class="form-control input-lg" name="password" id="password"  placeholder="input password" required>
-              <!-- <a href="lupapassword.php"><font color="#ffffff">Lupa Password?</font></a>
-             --></div>
-              
-               <button class="btn btn-warning btn-block btn-lg" type="submit" name= "masuk" style="background-color:#D6B70A;"><font color="#000000"><img src="assets/img/personkey-white.png">&nbsp<b>Login</b></font></button>
-
-          <br>
-          <center><font color="#000000"><small><em> Copyright &copy; Toko Buku Mayang </a></em></</small></font>
-          <br/>  
-           <font color="#000000"><?php echo date("Y"); ?></</small></font>
-            </center>
-          </form>
-
-          <?php
-          if(isset($koneksi, $_POST['masuk'])){
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-6">
+            <a href="#" class="btn btn-block btn-danger">
+          <i class="nav-icon fas fa-question-circle"></i> Lupa Password
+        </a>
+            
+          </div>
+          <!-- /.col -->
+          <div class="col-6">
+            <button type="submit" class="btn btn-primary btn-block" name="masuk">Log In <i class="nav-icon fas fa-sign-out-alt"></i></button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <?php
+        if(isset($koneksi, $_POST['masuk'])){
             $isianusername = trim(mysqli_escape_string($koneksi, $_POST['username']));
             $isianpw = sha1(trim(mysqli_escape_string($koneksi, $_POST['password'])));
             $cekdatabase = mysqli_query($koneksi, "SELECT * FROM tbl_pengguna WHERE username= '$isianusername' AND password= '$isianpw'") or die(mysqli_error($koneksi));
@@ -86,7 +82,7 @@
               echo '<script>
               window.location="../admin_dashboard"</script>';
             }
-            elseif($st_user==1){
+            else if($st_user==1){
 
               $_SESSION['username'] = $isianusername;
               $_SESSION['nama_user'] = $array['nama_user'];
@@ -108,15 +104,21 @@
             
            }
 
-          ?>
-
-        </div>
-      </div>
+      ?>
+          <br>
+          <center><font color="#000000"><small><em> Copyright &copy; Toko Buku Mayang </a></em></</small></font>
+          <br/> 
     </div>
+    <!-- /.login-card-body -->
   </div>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <script src="scripts/app.min.js"></script>
-</body>
+</div>
+<!-- /.login-box -->
 
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+</body>
 </html>
