d62f12cf6053baf81003a3acf6b946d22da92beb
NSyifaa <naylusyifa16@gmail.com>
2024-11-18 15:54:29 +0700

    database nyu

--- database/tokobuku .sql
+++ database/tokobuku .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 18 Nov 2024 pada 09.52
+-- Waktu pembuatan: 18 Nov 2024 pada 09.53
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 8.0.19
 
