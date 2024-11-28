49b8ec33d4ce612fc41ce226f30989354905fa3b
NSyifaa <naylusyifa16@gmail.com>
2024-11-28 10:24:08 +0700

    kasir dashboard

--- kasir_dashboard/index.php
+++ kasir_dashboard/index.php
@@ -7,6 +7,7 @@
   <?php
   session_start();
   $konstruktor ='kasir_dashboard';
+  require_once '../database/koneksi.php';
 
   if(isset($_SESSION['status'])){
     $status = $_SESSION['status'];
@@ -72,7 +73,77 @@
     <div class="content">
       <div class="container-fluid">
         <div class="row">
-         <?=$_SESSION['nama_user'];?>
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-success">
+            <div class="inner">
+              <h3>
+                <?php
+                // Ambil bulan dan tahun saat ini
+                $currentMonth = date('m');
+                $currentYear = date('Y');
+
+                // Query untuk menghitung total penjualan bulan ini
+                $queryTotalPenjualanBulanIni = mysqli_query($koneksi, "
+                  SELECT SUM(d.harga * d.qty) AS total_penjualan
+                  FROM tbl_detail_penjualan d
+                  JOIN tbl_penjualan p ON d.nomor = p.nomor
+                  WHERE MONTH(p.tgl) = '$currentMonth' AND YEAR(p.tgl) = '$currentYear'
+                ") or die(mysqli_error($koneksi));
+
+                // Ambil hasil query
+                $resultTotalPenjualanBulanIni = mysqli_fetch_assoc($queryTotalPenjualanBulanIni);
+
+                // Jika tidak ada hasil, set nilai total penjualan bulan ini menjadi 0
+                $totalPenjualanBulanIni = $resultTotalPenjualanBulanIni['total_penjualan'] ?? 0;
+
+                // Tampilkan total penjualan bulan ini dengan format Rupiah
+                echo 'Rp. ' . number_format($totalPenjualanBulanIni, 0, ',', '.');
+                ?>
+              </h3>
+              <p>Penjualan Bulan Ini  </p> 
+              (<?php echo date('F Y'); ?>)
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="../admin_penjualan" class="small-box-footer">Info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+          <div class="small-box bg-warning">
+            <div class="inner">
+              <h3>
+                <?php
+                // Query untuk menghitung total bruto bulan ini
+                $queryBrutoBulanIni = mysqli_query($koneksi, "
+                  SELECT SUM((d.harga - d.harga_beli) * d.qty) AS total_bruto
+                  FROM tbl_detail_penjualan d
+                  JOIN tbl_penjualan p ON d.nomor = p.nomor
+                  WHERE MONTH(p.tgl) = '$currentMonth' AND YEAR(p.tgl) = '$currentYear'
+                ") or die(mysqli_error($koneksi));
+
+                // Ambil hasil query
+                $brutoBulanIni = mysqli_fetch_assoc($queryBrutoBulanIni);
+
+                // Jika tidak ada hasil, set nilai total bruto bulan ini menjadi 0
+                $totalBrutoBulanIni = $brutoBulanIni['total_bruto'] ?? 0;
+
+                // Tampilkan total bruto bulan ini dengan format Rupiah
+                echo 'Rp. ' . number_format($totalBrutoBulanIni, 0, ',', '.');
+                ?>
+              </h3>
+              <p>Total Bruto Bulan Ini </p>
+              (<?php echo date('F Y'); ?>)
+            </div>
+            <div class="icon">
+              <i class="ion ion-cash"></i>
+            </div>
+            <a href="../admin_laporan_jual" class="small-box-footer">Info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        
         
 
         </div>
